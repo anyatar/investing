6ec03df6a092c6445dbf0f8b2013201b7eb61662
Anya Suraski <anya.suraski@gmail.com>
2019-07-10 18:08:56 +0300

    - Remove curl mode - Make base URL editable

--- test/test.php
+++ test/test.php
@@ -1,23 +1,16 @@
 <?php
 
+define("BASE_URL", "http://localhost:20081");
+
 include_once __DIR__.'/../module/mapper.php';
 
 $test = new Test();
 $results = $test->run();
 print_r($results);
 
-echo "Test is done. Go to the application (e.g. http://localhost:20081/chaos.html) to see the results and enjoy!\n";
+echo "Test is done. Go to the application (e.g. " . BASE_URL . "/chaos.html) to see the results and enjoy!\n";
 
 class Test {
-    
-    private $useCURL = true;
-    
-    public function __construct()
-    {
-        if (!function_exists('curl_exec')){
-            $this->useCURL = false;
-        }
-    }
     
     public function run() {
         
@@ -42,34 +35,16 @@ class Test {
     
     private function getResponseCode() {
         
-        $url = 'http://localhost:20080/response';
+        $url = BASE_URL . '/response';
         
-        if ($this->useCURL) {
-            
-            $ch = curl_init($url);
-            $response = curl_exec($ch);
-            
-            // Check HTTP status code
-            if (!curl_errno($ch)) {
-                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                
-                if ($httpCode == 503) {
-                    echo "Service unavalable\n";
-                    exit;
-                }
-                
-                return $httpCode;
-            }
+        @file_get_contents($url);
+        $statusLine = $http_response_header[0];
+        preg_match('{HTTP\/\S*\s(\d{3})}', $statusLine, $match);
+        if (isset($match[1])) {
+            return $match[1];
         } else {
-            @file_get_contents($url);
-            $statusLine = $http_response_header[0];
-            preg_match('{HTTP\/\S*\s(\d{3})}', $statusLine, $match);
-            if (isset($match[1])) {
-                return $match[1];
-            } else {
-                echo "Service unavalable\n";
-                exit;
-            }
+            echo "Service unavalable\n";
+            exit;
         }
     }
 }
